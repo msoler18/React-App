270c54144f663a8672777464784aab23211408e3
msoler18 <msoler735@gmail.com>
2018-05-08 18:30:04 -0500

    Change Welcome Page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Mi primera aplicación</h1> 
+        Esta es mi primera aplicación en React, está padrísimo!
+      </div>  
+    );
+  }
+}
+
+export default App;
